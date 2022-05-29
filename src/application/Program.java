c1398626b9a38f5d63de67ff5aaa4a24b60428d5
Antonio Alan <tonio.alan07@gmail.com>
2022-05-29 10:28:17 -0300

    First classes added

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p = new Position(7,12);
+        System.out.println(p);
+    }
+}
